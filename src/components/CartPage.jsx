fd041b0f5c94be88177fc717ead647efc646e322
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-19 10:23:59 -0400

    first

---
+++ src/components/CartPage.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Item from "./Item";
+import "/src/CartPage.css";
+
+function CartPage({ items, onAddOne, onRemoveOne }) {
+    return (
+      <ul className="CartPage-items">
+        {items.map((item) => (
+          <li key={item.id} className="CartPage-item">
+            <Item item={item}>
+              <div className="Item-controls">
+                <button
+                  className="Item-removeFromCart"
+                  onClick={() => onRemoveOne(item)}
+                >
+                  -
+                </button>
+                <span className="Item-quantity-box">
+                  <span className="Item-quantity">{item.count}</span>
+                </span>
+                <button className="Item-addToCart" onClick={() => onAddOne(item)}>
+                  +
+                </button>
+              </div>
+            </Item>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
+  CartPage.propTypes = {
+    items: PropTypes.array.isRequired,
+    onAddOne: PropTypes.func.isRequired,
+    onRemoveOne: PropTypes.func.isRequired,
+  };
+  
+  export default CartPage;
